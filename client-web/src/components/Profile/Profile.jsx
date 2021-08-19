6553ddc0458b900bf0018b2f1751211383023ba5
ummaeha <raya000@naver.com>
2021-08-20 04:51:56 +0900

    fix: too much re-render issue

--- client-web/src/components/Profile/Profile.jsx
+++ client-web/src/components/Profile/Profile.jsx
@@ -30,11 +30,11 @@ const Profile = ({userImg}) => {
             <span className="alertIcon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
             </span>
-            <img src={userImg === null ? window.location.origin+'/icons/user.svg': userImg} alt="" className="profileBtn menu-trigger" onClick={()=>onClick()} />
+            <img src={userImg === null ? window.location.origin+'/icons/user.svg': userImg} alt="" className="profileBtn menu-trigger" onClick={()=>(onClick())} />
             <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
                 <ul>
                     <li><span><a href="/settings">Settings</a></span></li>
-                    <li onClick={()=>onClick()}><LoginButton /></li>
+                    <li onClick={()=>(onClick())}><LoginButton /></li>
                 </ul>
             </nav>
         </div>
