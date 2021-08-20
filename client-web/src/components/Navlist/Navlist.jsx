43f9ae0aa21119e447f33de3a87567228df72b42
vvvpiano <vvvpiano60@gmail.com>
2021-08-20 17:43:24 +0900

    refactor: NavList 리팩토링

--- client-web/src/components/Navlist/Navlist.jsx
+++ client-web/src/components/Navlist/Navlist.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import "./Navlist.css";
 
-const Navlist = ({title, iconPath, open}) => {
-    const renderNavList = () => {
-        if (open) {
-            return (
-                <a className="navlist-container" href="#">
-                    <div className="navlist-flex-container">
-                        <img className="navlist-icon" src={iconPath}/>
-                        <div className="navlist-link-wrapper">
-                            <div className="navlist-link">{title}</div>
-                        </div>
-                        <div className="navlist-drop-button">▾</div>
-                    </div>
-                </a>
-            )
-        } else {
-            return (
-                <a className="navlist-container" href="#">
-                    <div className="navlist-flex-container">
-                        <img className="navlist-icon" src={iconPath} alt="navlist-icon"/>
-                        <div className="navlist-link-wrapper" style={{width:"0px", paddingRight:"0px"}}>
-                            <div className="navlist-link" style={{visibility:"hidden"}}>{title}</div>
-                        </div>
-                        <div className="navlist-drop-button">▾</div>
-                    </div>
-                </a>
-            )
-        }
+const Navlist = ({title, iconPath, dropbuttonPath, open}) => {
+
+    const renderIconImage = () => {
+        return iconPath ? <img className="navlist-icon" src={iconPath} alt="navlist-icon"/> : null;
     }
+
+    const renderWrapperStyle = () => {
+        return open ? {} : {width:"0px", paddingRight:"0px"};
+    }
+
+    const renderLinkStyle = () => {
+        return open ? {} : {visibility:"hidden"};
+    }
+
+    // [TODO] icon 에셋 저장하면 이 부분 활성화시키기
+    // const renderDropButton = () => {
+    //     return dropbuttonPath ? <img className="navlist-drop-button" src={dropbuttonPath} alt="dropdown-button" /> : null;
+    // }
+
+    const renderNavList = () => {
+        return (
+            <a className="navlist-container" href="#">
+                <div className="navlist-flex-container">
+                    {renderIconImage()}
+                    <div className="navlist-link-wrapper" style={renderWrapperStyle()}>
+                        <div className="navlist-link" style={renderLinkStyle()}>{title}</div>
+                    </div>
+                    {/* {renderDropButton()} */}
+                    <div className="navlist-drop-button">▾</div>
+                </div>
+            </a>
+        )
+    }
+
     return (
         renderNavList()
     )
