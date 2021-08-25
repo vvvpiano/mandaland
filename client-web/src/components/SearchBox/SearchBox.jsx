b753f502ff017f1e633a05a86aeab870266a4acf
ummaeha <raya000@naver.com>
2021-08-25 11:07:06 +0900

    add: header 바 검색창 이벤트 삽입

--- client-web/src/components/SearchBox/SearchBox.jsx
+++ client-web/src/components/SearchBox/SearchBox.jsx
@@ -8,34 +8,21 @@ const enterkey = (e) => {
         // 엔터키가 눌렸을 때 실행할 내용
         console.log(e.target.value) //input태그에 들어온 내용
     }
+}   
+const clickSearch = (e) => {
+    console.log(e.target.value);
 }
 
 const SearchBox = () => {
     return (
         <div className="inputBoxAlign">
             <div className="inputBoxWrapper">
-                <select name="" id="" className="selectBoxStyle">
+                <select name="" id="" className="selectBoxStyle pixel">
                     <option value="">주제</option>
                     <option value="">사용자</option>
                 </select>
                 <input type="text" placeholder="검색어를 입력하세요" className="inputBox" onKeyUp={(e) => enterkey(e)} />
-                <Link to="#">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="feather feather-search searchIconStyle"
-                    >
-                        <circle cx="11" cy="11" r="8"></circle>
-                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                    </svg>
-                </Link>
+                <img src={window.location.origin + "/icons/search.svg"} alt="searchButton" className="searchIconStyle" onClick={(e) => clickSearch(e)}/>
             </div>
         </div>
     )
